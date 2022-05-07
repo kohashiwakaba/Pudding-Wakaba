06eeb794e676994645a5dc792bb100edf5d55684
wakaba (aka. 미카) <kanade.tinkle@gmail.com>
2022-05-08 01:49:31 +0900

    Update wisps.lua

--- scripts/wisps.lua
+++ scripts/wisps.lua
@@ -11,7 +11,7 @@ function wakaba:wispDeathCheck(wisp)
         player:AddMinisaac(player.Position)
         player:AddMinisaac(player.Position)
       elseif wisp.SubType == wakaba.COLLECTIBLE_GRIMREAPER_DEFENDER then
-        player:UseActiveItem(CollectibleType.COLLECTIBLE_NECRONOMICON, UseFlag.USE_NOANIM, -1)
+        player:UseActiveItem(CollectibleType.COLLECTIBLE_NECRONOMICON, UseFlag.USE_NOANIM | UseFlag.USE_VOID, -1)
       elseif wisp.SubType == wakaba.COLLECTIBLE_ISEKAI_DEFINITION then
         local clot = Isaac.Spawn(EntityType.ENTITY_FAMILIAR, FamiliarVariant.BLOOD_BABY, 3, wisp.Position, Vector.Zero, player):ToFamiliar()
       end
