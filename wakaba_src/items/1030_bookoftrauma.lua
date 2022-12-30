f76cf19cb63d8bc77d689261ab217019a80e1c7a
wakaba (aka. 미카) <kanade.tinkle@gmail.com>
2022-12-30 23:06:24 +0900

    trauma tweaks

--- wakaba_src/items/1030_bookoftrauma.lua
+++ wakaba_src/items/1030_bookoftrauma.lua
@@ -1,9 +1,20 @@
+local isc = require("wakaba_src.libs.isaacscript-common")
 function wakaba:ItemUse_BookOfTrauma(_, rng, player, useFlags, activeSlot, varData)
+	local i = 0
+	local allowOtherTears = false
 	local entities = Isaac.FindByType(EntityType.ENTITY_TEAR)
-	for _, e in ipairs(entities) do
-		wakaba.G:BombExplosionEffects(e.Position, player.Damage * 8 + 20, TearFlags.TEAR_BRIMSTONE_BOMB, Color.Default, player, 1, true, false, DamageFlag.DAMAGE_EXPLOSION | DamageFlag.DAMAGE_IGNORE_ARMOR)
-		e:Remove()
+	if #entities > 0 then
+		for _, e in ipairs(entities) do
+			if i < 15 and (allowOtherTears or (e.FrameCount > 0 and e:ToTear() and isc:isTearFromFamiliar(e:ToTear()))) then
+				wakaba.G:BombExplosionEffects(e.Position, player.Damage * 8 + 20, TearFlags.TEAR_BRIMSTONE_BOMB, Color.Default, player, 1, true, false, DamageFlag.DAMAGE_EXPLOSION | DamageFlag.DAMAGE_IGNORE_ARMOR)
+				e:Remove()
+				i = i + 1
+			elseif i >= 15 then
+				finished = true
+			end
+		end
 	end
+	wakaba.G:BombExplosionEffects(player.Position, player.Damage * 8 + 20, TearFlags.TEAR_BRIMSTONE_BOMB, Color.Default, player, 1, true, false, DamageFlag.DAMAGE_EXPLOSION | DamageFlag.DAMAGE_IGNORE_ARMOR)
 
 	if not (useFlags & UseFlag.USE_NOANIM == UseFlag.USE_NOANIM) then
 		player:AnimateCollectible(wakaba.Enums.Collectibles.BOOK_OF_TRAUMA, "UseItem", "PlayerPickup")
