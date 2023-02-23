d2f922e863547e2a3f2fb11fae01ba60b7b35f25
wakaba (aka. 미카) <37092106+kohashiwakaba@users.noreply.github.com>
2023-02-23 18:05:28 +0900

    more item ideas

--- wakaba_src/items/1071_richerdivider.lua
+++ wakaba_src/items/1071_richerdivider.lua
@@ -1,5 +1,5 @@
 --[[ 
-	Richer's Divider (리셰의 분배기) - 액티브(Active) - 6 rooms
-	
+	Richer's Divider (리셰의 분배기) - 액티브(Active) - 3 rooms
+	사용 시 카드/알약 및 폭탄/열쇠를 반전
  ]]
 local isc = require("wakaba_src.libs.isaacscript-common")
---
+++ wakaba_src/items/1073_wakabaswimsuit.lua
@@ -0,0 +1,6 @@
+--[[ 
+	Wakaba's Swimsuit - 와카바의 수영복 (패시브)
+	연사 +0.2, 주변의 적 장판을 제거
+ ]]
+
+local isc = require("wakaba_src.libs.isaacscript-common")
