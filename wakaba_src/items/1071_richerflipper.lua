6c4ebdab75844aa0caa5cac5b04f4d0e4124b9fc
wakaba (aka. 미카) <37092106+kohashiwakaba@users.noreply.github.com>
2023-02-27 17:03:46 +0900

    flipper

---
+++ wakaba_src/items/1071_richerflipper.lua
@@ -0,0 +1,27 @@
+--[[ 
+	Richer's Flipper (리셰의 반전기) - 액티브(Active) - 3 rooms
+	사용 시 카드/알약 및 폭탄/열쇠를 반전
+ ]]
+local isc = require("wakaba_src.libs.isaacscript-common")
+
+function wakaba:ItemUse_RicherFlipper(_, rng, player, useFlags, activeSlot, varData)
+	local pickups = isc:getEntities(EntityType.ENTITY_PICKUP)
+	for _, pickup in ipairs(pickups) do
+		if pickup.Variant == PickupVariant.PICKUP_BOMB then
+			pickup:Morph(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_KEY, 0, false, false, true)
+		elseif pickup.Variant == PickupVariant.PICKUP_KEY then
+			pickup:Morph(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_BOMB, 0, false, false, true)
+		elseif pickup.Variant == PickupVariant.PICKUP_TAROTCARD then
+			pickup:Morph(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_PILL, 0, false, false, true)
+		elseif pickup.Variant == PickupVariant.PICKUP_PILL then
+			pickup:Morph(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_TAROTCARD, 0, false, false, true)
+		end
+	end
+
+	return {
+		Discharge = true,
+		Remove = false,
+		ShowAnim = true,
+	}
+end
+wakaba:AddCallback(ModCallbacks.MC_USE_ITEM, wakaba.ItemUse_RicherFlipper, wakaba.Enums.Collectibles.RICHERS_FLIPPER)
