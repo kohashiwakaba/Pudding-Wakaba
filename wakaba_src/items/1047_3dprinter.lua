3b184afa036af9a2aeb9ce8b0cf3bf8aa820fcc1
wakaba (aka. 미카) <kanade.tinkle@gmail.com>
2023-05-17 02:13:14 +0900

    3d printer drops trinket on full charge

--- wakaba_src/items/1047_3dprinter.lua
+++ wakaba_src/items/1047_3dprinter.lua
@@ -1,3 +1,4 @@
+local isc = require("wakaba_src.libs.isaacscript-common")
 function wakaba:ItemUse_3dPrinter(_, rng, player, useFlags, activeSlot, varData)
 
 	--get the trinkets they're currently holding
@@ -23,3 +24,21 @@ function wakaba:ItemUse_3dPrinter(_, rng, player, useFlags, activeSlot, varData)
 	end
 end
 wakaba:AddCallback(ModCallbacks.MC_USE_ITEM, wakaba.ItemUse_3dPrinter, wakaba.Enums.Collectibles._3D_PRINTER)
+
+
+function wakaba:RoomClear_3dPrinter(rng, spawnPosition)
+
+  for i = 1, wakaba.G:GetNumPlayers() do
+		local player = Isaac.GetPlayer(i - 1)
+		for i = 0, 2 do
+			if player:GetActiveItem(i) == wakaba.Enums.Collectibles._3D_PRINTER then
+				local config = Isaac.GetItemConfig():GetCollectible(wakaba.Enums.Collectibles._3D_PRINTER)
+				if player:GetActiveCharge(i) == config.MaxCharges then
+					local dreamcard = Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_TRINKET, wakaba.G:GetItemPool():GetTrinket(), Isaac.GetFreeNearPosition(player.Position, 0.0), Vector(0,0), nil):ToPickup()
+				end
+			end
+		end
+	end
+end
+wakaba:AddCallback(ModCallbacks.MC_PRE_SPAWN_CLEAN_AWARD, wakaba.RoomClear_3dPrinter)
+wakaba:AddCallbackCustom(isc.ModCallbackCustom.POST_GREED_MODE_WAVE, wakaba.RoomClear_3dPrinter)
