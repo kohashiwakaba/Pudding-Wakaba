5e731df3e38ab4d9742a6cb4f75fad7b73a91a7a
wakaba (aka. 미카) <kanade.tinkle@gmail.com>
2023-06-11 20:20:50 +0900

    retribution compat for everlasting pills

--- wakaba_src/compat/retribution.lua
+++ wakaba_src/compat/retribution.lua
@@ -1,7 +1,22 @@
 local rtReplaced = false
 function wakaba:GameStart_RetributionCompat()
 	if Retribution and not rtReplaced then
+		local mod = Retribution
+		-- Magnet Heaven
 		wakaba:AddCallback(ModCallbacks.MC_POST_PICKUP_UPDATE, wakaba.PickupUpdate_Magnet, Retribution.PICKUPS.SPOILS_COIN)
+
+		-- Ret - Everlasting Pills
+		table.insert(mod.EverlastingPills, wakaba.Enums.Pills.TROLLED)
+		table.insert(mod.EverlastingPills, wakaba.Enums.Pills.TO_THE_START)
+		table.insert(mod.EverlastingPills, wakaba.Enums.Pills.EXPLOSIVE_DIARRHEA_2)
+		table.insert(mod.EverlastingPills, wakaba.Enums.Pills.EXPLOSIVE_DIARRHEA_2_NOT)
+		table.insert(mod.EverlastingPills, wakaba.Enums.Pills.SOCIAL_DISTANCE)
+		table.insert(mod.EverlastingPills, wakaba.Enums.Pills.PRIEST_BLESSING)
+		table.insert(mod.EverlastingPills, wakaba.Enums.Pills.UNHOLY_CURSE)
+		table.insert(mod.EverlastingPills, wakaba.Enums.Pills.FLAME_PRINCESS)
+		table.insert(mod.EverlastingPills, wakaba.Enums.Pills.FIREY_TOUCH)
+
+
 
 		rtReplaced = true
 	end
