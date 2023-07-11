e6d29395b8abec47f068d96a62449501efc93eb9
wakaba (aka. 미카) <kanade.tinkle@gmail.com>
2023-07-11 15:46:43 +0900

    Return Token reverts timer to 1 sec instead of 0
    - to fix potential issues with mods using slugcat's save manager

--- wakaba_src/pickups/2008_returntoken.lua
+++ wakaba_src/pickups/2008_returntoken.lua
@@ -1,6 +1,6 @@
 function wakaba:UseCard_ReturnToken(_, player, flags)
 	player:UseCard(Card.CARD_REVERSE_FOOL, UseFlag.USE_MIMIC | UseFlag.USE_NOHUD | UseFlag.USE_NOANNOUNCER | UseFlag.USE_CARBATTERY | UseFlag.USE_NOANIM)
 	player:UseActiveItem(CollectibleType.COLLECTIBLE_R_KEY, UseFlag.USE_NOANIM | UseFlag.USE_VOID, -1)
-	wakaba.G.TimeCounter = 0
+	wakaba.G.TimeCounter = 30
 end
 wakaba:AddCallback(ModCallbacks.MC_USE_CARD, wakaba.UseCard_ReturnToken, wakaba.Enums.Cards.CARD_RETURN_TOKEN)
