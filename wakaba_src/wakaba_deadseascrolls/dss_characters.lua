6a99cb3e98d46e1d37a0c15d025847a5862c157d
wakaba (aka. 미카) <kanade.tinkle@gmail.com>
2023-07-19 22:48:39 +0900

    Update dss_characters.lua

--- wakaba_src/wakaba_deadseascrolls/dss_characters.lua
+++ wakaba_src/wakaba_deadseascrolls/dss_characters.lua
@@ -2,22 +2,22 @@ local wakabadirectory = wakaba.DSS_DIRECTORY
 local dssmod = wakaba.DSS_MOD
 
 wakabadirectory.wakabainfos = {
-	format = {
-		Panels = {
-			{
-				Panel = dssmod.panels.main,
-				Offset = Vector(-50, 0),
-				Color = Color.Default,
-			},
-			{
-					Panel = dssmod.panels.tooltip,
-					Offset = Vector(180, -2),
-					Color = 1
-			}
-		}
-	},
-	generate = function()
-	end,
-	gridx = maxCol,
-	buttons = {}
+  format = {
+    Panels = {
+      {
+        Panel = dssmod.panels.main,
+        Offset = Vector(-50, 0),
+        Color = Color.Default,
+      },
+      {
+          Panel = dssmod.panels.tooltip,
+          Offset = Vector(180, -2),
+          Color = 1
+      }
+    }
+  },
+  generate = function()
+  end,
+  gridx = maxCol,
+  buttons = {}
 }
