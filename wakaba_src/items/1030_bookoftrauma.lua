d760723c1694e33524df5ebdd9ebde960a1e011e
wakaba (aka. 미카) <37092106+kohashiwakaba@users.noreply.github.com>
2023-08-02 14:52:43 +0900

    epiph : golden book of trauma

--- wakaba_src/items/1030_bookoftrauma.lua
+++ wakaba_src/items/1030_bookoftrauma.lua
@@ -2,12 +2,13 @@ local isc = require("wakaba_src.libs.isaacscript-common")
 function wakaba:ItemUse_BookOfTrauma(_, rng, player, useFlags, activeSlot, varData)
 	local i = 0
 	local max = wakaba.Enums.Constants.MAX_TRAUMA_COUNT
+	local isGolden = Epiphany and Epiphany.API and Epiphany.API:IsGoldenItem(item)
 	local allowOtherTears = false
 	local entities = Isaac.FindByType(EntityType.ENTITY_TEAR)
 	if #entities > 0 then
 		for _, e in ipairs(entities) do
 			if i < max and (allowOtherTears or GetPtrHash(isc:getPlayerFromEntity(e)) == GetPtrHash(player)) then
-				wakaba.G:BombExplosionEffects(e.Position, player.Damage * 8 + 20, TearFlags.TEAR_BRIMSTONE_BOMB, Color.Default, player, 1, true, false, DamageFlag.DAMAGE_EXPLOSION | DamageFlag.DAMAGE_IGNORE_ARMOR)
+				wakaba.G:BombExplosionEffects(e.Position, player.Damage * 8 * (isGolden and 1.3 or 1) + 20, TearFlags.TEAR_BRIMSTONE_BOMB | (isGolden and TearFlags.TEAR_GIGA_BOMB or 0), Color.Default, player, (isGolden and 2 or 1), true, false, DamageFlag.DAMAGE_EXPLOSION | DamageFlag.DAMAGE_IGNORE_ARMOR)
 				e:Remove()
 				i = i + 1
 			elseif i >= max then
@@ -15,7 +16,7 @@ function wakaba:ItemUse_BookOfTrauma(_, rng, player, useFlags, activeSlot, varDa
 			end
 		end
 	end
-	wakaba.G:BombExplosionEffects(player.Position, player.Damage * 8 + 20, TearFlags.TEAR_BRIMSTONE_BOMB, Color.Default, player, 1, true, false, DamageFlag.DAMAGE_EXPLOSION | DamageFlag.DAMAGE_IGNORE_ARMOR)
+	wakaba.G:BombExplosionEffects(player.Position, player.Damage * 8 * (isGolden and 1.3 or 1) + 20, TearFlags.TEAR_BRIMSTONE_BOMB | (isGolden and TearFlags.TEAR_GIGA_BOMB or 0), Color.Default, player, (isGolden and 2 or 1), true, false, DamageFlag.DAMAGE_EXPLOSION | DamageFlag.DAMAGE_IGNORE_ARMOR)
 
 	if not (useFlags & UseFlag.USE_NOANIM == UseFlag.USE_NOANIM) then
 		player:AnimateCollectible(wakaba.Enums.Collectibles.BOOK_OF_TRAUMA, "HideItem", "PlayerPickup")
