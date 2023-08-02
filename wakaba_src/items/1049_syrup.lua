1ff98d382301decc996de3239e495336263ad82c
wakaba (aka. 미카) <37092106+kohashiwakaba@users.noreply.github.com>
2023-08-02 14:44:46 +0900

    epiph : golden syrup

--- wakaba_src/items/1049_syrup.lua
+++ wakaba_src/items/1049_syrup.lua
@@ -1,10 +1,8 @@
 function wakaba:Cache_Syrup(player, cacheFlag)
 	if player:HasCollectible(wakaba.Enums.Collectibles.SYRUP) then
+		local isGolden = Epiphany and Epiphany.API and Epiphany.API:IsGoldenItem(wakaba.Enums.Collectibles.SYRUP)
 		if cacheFlag & CacheFlag.CACHE_DAMAGE == CacheFlag.CACHE_DAMAGE then
 			player.Damage = player.Damage + (1.25 * wakaba:getEstimatedDamageMult(player))
-		end
-		if cacheFlag & CacheFlag.CACHE_SPEED == CacheFlag.CACHE_SPEED then
-			player.MoveSpeed = player.MoveSpeed * 0.9
 		end
     if cacheFlag & CacheFlag.CACHE_FLYING == CacheFlag.CACHE_FLYING then
       player.CanFly = true
@@ -12,6 +10,15 @@ function wakaba:Cache_Syrup(player, cacheFlag)
     if cacheFlag & CacheFlag.CACHE_RANGE == CacheFlag.CACHE_RANGE then
       player.TearRange = player.TearRange + (40 * 3)
     end
+		if isGolden then
+			if cacheFlag == CacheFlag.CACHE_TEARFLAG then
+				player.TearFlags = player.TearFlags | TearFlags.TEAR_SPECTRAL
+			end
+		else
+			if cacheFlag & CacheFlag.CACHE_SPEED == CacheFlag.CACHE_SPEED then
+				player.MoveSpeed = player.MoveSpeed * 0.9
+			end
+		end
 	end
 end
 
