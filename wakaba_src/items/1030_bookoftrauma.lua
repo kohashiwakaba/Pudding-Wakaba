611ae5769d0eb077b82234874f06ecacc91c817b
wakaba (aka. 미카) <kanade.tinkle@gmail.com>
2023-08-04 07:53:49 +0900

    golden book of trauma no longer explodes with brimstone

--- wakaba_src/items/1030_bookoftrauma.lua
+++ wakaba_src/items/1030_bookoftrauma.lua
@@ -8,7 +8,7 @@ function wakaba:ItemUse_BookOfTrauma(item, rng, player, useFlags, activeSlot, va
 	if #entities > 0 then
 		for _, e in ipairs(entities) do
 			if i < max and (allowOtherTears or GetPtrHash(isc:getPlayerFromEntity(e)) == GetPtrHash(player)) then
-				wakaba.G:BombExplosionEffects(e.Position, player.Damage * 8 * (isGolden and 1.3 or 1) + 20, TearFlags.TEAR_BRIMSTONE_BOMB | (isGolden and TearFlags.TEAR_GIGA_BOMB or 0), Color.Default, player, (isGolden and 2 or 1), true, false, DamageFlag.DAMAGE_EXPLOSION | DamageFlag.DAMAGE_IGNORE_ARMOR)
+				wakaba.G:BombExplosionEffects(e.Position, player.Damage * 8 * (isGolden and 1.3 or 1) + 20, (isGolden and TearFlags.TEAR_GIGA_BOMB or TearFlags.TEAR_BRIMSTONE_BOMB), Color.Default, player, 1, true, false, DamageFlag.DAMAGE_EXPLOSION | DamageFlag.DAMAGE_IGNORE_ARMOR)
 				e:Remove()
 				i = i + 1
 			elseif i >= max then
