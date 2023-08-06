847276c176bc668999216b4cffbd32db1a963af2
wakaba (aka. 미카) <kanade.tinkle@gmail.com>
2023-08-06 21:02:21 +0900

    plasma beam no longer damage fireplace

--- wakaba_src/items/1060_plasmabeam.lua
+++ wakaba_src/items/1060_plasmabeam.lua
@@ -1,18 +1,20 @@
 function wakaba:PlasmaBeamDamage(source, target, data, newDamage, newFlags, isAlreadyLaser)
 	local returndata = {}
 	local num = 0
-	for i = 0, wakaba.G:GetNumPlayers() - 1 do
-		local player = Isaac.GetPlayer(i)
-		num = num + player:GetCollectibleNum(wakaba.Enums.Collectibles.PLASMA_BEAM) + player:GetEffects():GetCollectibleEffectNum(wakaba.Enums.Collectibles.PLASMA_BEAM)
-	end
-	if num > 0 then
-		returndata.newDamage = newDamage * (1.25 ^ num)
-		returndata.sendNewDamage = true
-		returndata.newFlags = newFlags | DamageFlag.DAMAGE_LASER
-		if isAlreadyLaser then
-			returndata.newFlags = returndata.newFlags | DamageFlag.DAMAGE_IGNORE_ARMOR
+	if target.Type ~= EntityType.ENTITY_FIREPLACE then
+		for i = 0, wakaba.G:GetNumPlayers() - 1 do
+			local player = Isaac.GetPlayer(i)
+			num = num + player:GetCollectibleNum(wakaba.Enums.Collectibles.PLASMA_BEAM) + player:GetEffects():GetCollectibleEffectNum(wakaba.Enums.Collectibles.PLASMA_BEAM)
 		end
-		SFXManager():Play(SoundEffect.SOUND_REDLIGHTNING_ZAP_WEAK)
+		if num > 0 then
+			returndata.newDamage = newDamage * (1.25 ^ num)
+			returndata.sendNewDamage = true
+			returndata.newFlags = newFlags | DamageFlag.DAMAGE_LASER
+			if isAlreadyLaser then
+				returndata.newFlags = returndata.newFlags | DamageFlag.DAMAGE_IGNORE_ARMOR
+			end
+			SFXManager():Play(SoundEffect.SOUND_REDLIGHTNING_ZAP_WEAK)
+		end
 	end
 	return returndata
 end
