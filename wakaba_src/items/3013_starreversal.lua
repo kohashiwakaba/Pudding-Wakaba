b6cd9c19b3f965916034b95faaf17cd2e1a172ae
wakaba (aka. 미카) <37092106+kohashiwakaba@users.noreply.github.com>
2023-09-25 14:11:39 +0900

    star reversal : smelted ones now use mute button to drop

--- wakaba_src/items/3013_starreversal.lua
+++ wakaba_src/items/3013_starreversal.lua
@@ -1,34 +1,57 @@
 --[[ function wakaba:NewRoom_StarReversal()
-  local room = wakaba.G:GetRoom()
-  for i = 0, wakaba.G:GetNumPlayers() -1 do
-    if player:HasTrinket(wakaba.Enums.Trinkets.STAR_REVERSAL) then
-      Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, wakaba.G:GetItemPool():GetCollectible(ItemPoolType.POOL_PLANETARIUM), Isaac.GetFreeNearPosition(room:GetCenterPos(), 40.0), Vector.Zero, nil)
-      player:RemoveTrinket(wakaba.Enums.Trinkets.STAR_REVERSAL)
-    end
-  end
+	local room = wakaba.G:GetRoom()
+	for i = 0, wakaba.G:GetNumPlayers() -1 do
+		if player:HasTrinket(wakaba.Enums.Trinkets.STAR_REVERSAL) then
+			Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, wakaba.G:GetItemPool():GetCollectible(ItemPoolType.POOL_PLANETARIUM), Isaac.GetFreeNearPosition(room:GetCenterPos(), 40.0), Vector.Zero, nil)
+			player:RemoveTrinket(wakaba.Enums.Trinkets.STAR_REVERSAL)
+		end
+	end
 
 end
 wakaba:AddCallback(ModCallbacks.MC_POST_NEW_ROOM, wakaba.NewRoom_StarReversal) ]]
 
 local sfx = SFXManager()
+
+local function shouldActivateReversal()
+	local room = wakaba.G:GetRoom()
+	return room:GetType() == RoomType.ROOM_TREASURE or wakaba:IsValidWakabaRoom(Game():GetLevel():GetCurrentRoomDesc(), wakaba.RoomTypes.WINTER_ALBIREO)
+end
+
 function wakaba:PickupUpdate_StarReversal(trinket)
-  if trinket.SubType % 32768 ~= wakaba.Enums.Trinkets.STAR_REVERSAL or trinket.Touched ~= true then return end
-  local sprite = trinket:GetSprite()
-  local room = wakaba.G:GetRoom()
-  if room:GetType() == RoomType.ROOM_TREASURE or wakaba:IsValidWakabaRoom(Game():GetLevel():GetCurrentRoomDesc(), wakaba.RoomTypes.WINTER_ALBIREO) then
-    if sprite:IsEventTriggered("DropSound") then
-      local newPos = Isaac.GetFreeNearPosition(trinket.Position, 40.0)
-      if trinket.SubType > 32768 then
-        sfx:Play(SoundEffect.SOUND_SOUL_PICKUP, 1, 0, false, 1)
-        Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, wakaba.G:GetItemPool():GetCollectible(ItemPoolType.POOL_PLANETARIUM), newPos, Vector.Zero, nil)
-        Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, wakaba.G:GetItemPool():GetCollectible(ItemPoolType.POOL_PLANETARIUM), newPos, Vector.Zero, nil)
-        trinket:Remove()
-      else
-        sfx:Play(SoundEffect.SOUND_SOUL_PICKUP, 1, 0, false, 1)
-        Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, wakaba.G:GetItemPool():GetCollectible(ItemPoolType.POOL_PLANETARIUM), newPos, Vector.Zero, nil)
-        trinket:Remove()
-      end
-    end
-  end
+	if trinket.SubType % 32768 ~= wakaba.Enums.Trinkets.STAR_REVERSAL or trinket.Touched ~= true then return end
+	local sprite = trinket:GetSprite()
+	local room = wakaba.G:GetRoom()
+	if shouldActivateReversal() then
+		if sprite:IsEventTriggered("DropSound") then
+			local newPos = Isaac.GetFreeNearPosition(trinket.Position, 40.0)
+			if trinket.SubType > 32768 then
+				sfx:Play(SoundEffect.SOUND_SOUL_PICKUP, 1, 0, false, 1)
+				Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, wakaba.G:GetItemPool():GetCollectible(ItemPoolType.POOL_PLANETARIUM), newPos, Vector.Zero, nil)
+				Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, wakaba.G:GetItemPool():GetCollectible(ItemPoolType.POOL_PLANETARIUM), newPos, Vector.Zero, nil)
+				trinket:Remove()
+			else
+				sfx:Play(SoundEffect.SOUND_SOUL_PICKUP, 1, 0, false, 1)
+				Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, wakaba.G:GetItemPool():GetCollectible(ItemPoolType.POOL_PLANETARIUM), newPos, Vector.Zero, nil)
+				trinket:Remove()
+			end
+		end
+	end
 end
 wakaba:AddCallback(ModCallbacks.MC_POST_PICKUP_UPDATE, wakaba.PickupUpdate_StarReversal, PickupVariant.PICKUP_TRINKET)
+
+function wakaba:PlayerUpdate_StarReversal(player)
+	if not player:IsHoldingItem() and wakaba:PlayerHasSmeltedTrinket(player, wakaba.Enums.Trinkets.STAR_REVERSAL) then
+		local room = wakaba.G:GetRoom()
+		if Input.IsActionPressed(ButtonAction.ACTION_MUTE, player.ControllerIndex) and shouldActivateReversal() then
+			local newPos = Isaac.GetFreeNearPosition(player.Position, 40.0)
+			local oldTrinketCount = player:GetTrinketMultiplier(wakaba.Enums.Trinkets.STAR_REVERSAL)
+			player:TryRemoveTrinket(wakaba.Enums.Trinkets.STAR_REVERSAL)
+			local newTrinketCount = player:GetTrinketMultiplier(wakaba.Enums.Trinkets.STAR_REVERSAL)
+			for i = 1, (oldTrinketCount - newTrinketCount) do
+				sfx:Play(SoundEffect.SOUND_SOUL_PICKUP, 1, 0, false, 1)
+				Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, wakaba.G:GetItemPool():GetCollectible(ItemPoolType.POOL_PLANETARIUM), newPos, Vector.Zero, nil)
+			end
+		end
+	end
+end
+wakaba:AddCallback(ModCallbacks.MC_POST_PLAYER_UPDATE, wakaba.PlayerUpdate_StarReversal)
