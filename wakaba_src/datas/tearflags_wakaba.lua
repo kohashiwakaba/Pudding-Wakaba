72e7f09b47d4cb8ede75c5ce1b4d4ee1e20fa0f3
wakaba (aka. 미카) <37092106+kohashiwakaba@users.noreply.github.com>
2023-10-06 16:23:16 +0900

    rira : nerf gun preparation

--- wakaba_src/datas/tearflags_wakaba.lua
+++ wakaba_src/datas/tearflags_wakaba.lua
@@ -2,4 +2,5 @@ wakaba.TearFlag = {
 	EXECUTE = 1 << 0, -- Tear applies the Execute status effect (used for Executioner)
 	AQUA = 1 << 1, -- Tear applies Aqua status effect (used for Rira)
 	ZIPPED = 1 << 2, -- Tear applies Zipped status effect (used for Black Bean Mochi)
+	NERF = 1 << 3, -- Tear applies Weakness status effect (used for Nerf Gun)
 }
