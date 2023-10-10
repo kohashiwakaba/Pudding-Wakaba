5671e12fa0dcfafe90153f6a287375692e4f3627
wakaba (aka. 미카) <37092106+kohashiwakaba@users.noreply.github.com>
2023-10-10 18:03:47 +0900

    rira : test

--- wakaba_src/items/0013_chimaki.lua
+++ wakaba_src/items/0013_chimaki.lua
@@ -5,3 +5,33 @@
  ]]
 
 local isc = require("wakaba_src.libs.isaacscript-common")
+
+function wakaba:RoomGen_Chimaki_BossDoor()
+	local level = wakaba.G:GetLevel()
+	if level:GetAbsoluteStage() == LevelStage.STAGE6 then
+		local roomData = isc:getRoomDataForTypeVariant(RoomType.ROOM_BOSS, 42000)
+		local writeableRoom = level:GetRoomByIdx(-14, -1)
+		local writeablePrevRoom = level:GetRoomByIdx(-3, -1)
+		writeableRoom.Data = roomData
+		writeableRoom.SpawnSeed = writeablePrevRoom.SpawnSeed
+		writeableRoom.DecorationSeed = writeablePrevRoom.DecorationSeed
+		writeableRoom.AwardSeed = writeablePrevRoom.AwardSeed
+	end
+end
+wakaba:AddPriorityCallback(ModCallbacks.MC_POST_NEW_LEVEL, CallbackPriority.EARLY, wakaba.RoomGen_Chimaki_BossDoor)
+
+function wakaba:RoomClear_Chimaki_BossDoor(rng, spawnPosition)
+	local level = wakaba.G:GetLevel()
+	local room = wakaba.G:GetRoom()
+	local bossID = room:GetBossID()
+	if level:GetAbsoluteStage() == LevelStage.STAGE6 and (bossID == 40 or bossID == 54) and room:IsClear() then
+		--room:TrySpawnMegaSatanRoomDoor(true)
+		room:TrySpawnBlueWombDoor(true, true, true)
+		local doors = isc:getDoorsToRoomIndex(-8)
+		for _, door in ipairs(doors) do
+			door.TargetRoomIndex = -14
+		end
+	end
+end
+wakaba:AddCallback(ModCallbacks.MC_PRE_SPAWN_CLEAN_AWARD, wakaba.RoomClear_Chimaki_BossDoor)
+wakaba:AddPriorityCallback(ModCallbacks.MC_POST_NEW_ROOM, CallbackPriority.EARLY, wakaba.RoomClear_Chimaki_BossDoor)
