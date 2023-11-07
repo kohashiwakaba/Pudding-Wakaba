fff6283744c7a6df6dd497bdff349ed943c14e0e
wakaba (aka. 미카) <37092106+kohashiwakaba@users.noreply.github.com>
2023-11-07 17:53:11 +0900

    rira : nerf gun reserve

--- wakaba_src/items/1084_nerfgun.lua
+++ wakaba_src/items/1084_nerfgun.lua
@@ -3,3 +3,12 @@
 	공격방향으로 너프 샷을 여러 번 발사
  ]]
 local isc = require("wakaba_src.libs.isaacscript-common")
+
+wakaba:AddCallback(wakaba.Callback.APPLY_TEARFLAG_EFFECT, function(_, effectTarget, player, effectSource)
+	if wakaba:CanApplyStatusEffect(effectTarget) then
+		effectTarget:AddEntityFlags(EntityFlag.FLAG_WEAKNESS)
+		wakaba:scheduleForUpdate(function()
+			effectTarget:ClearEntityFlags(EntityFlag.FLAG_WEAKNESS)
+		end, 150)
+	end
+end, wakaba.TearFlag.NERF)
