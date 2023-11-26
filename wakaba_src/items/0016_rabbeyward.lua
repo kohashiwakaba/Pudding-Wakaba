c7dafee78781b2516337371d368d3c8cd64ba52a
wakaba (aka. 미카) <kanade.tinkle@gmail.com>
2023-11-26 19:54:44 +0900

    rira : more item reserves

---
+++ wakaba_src/items/0016_rabbeyward.lua
@@ -0,0 +1,8 @@
+--[[ 
+	Rabbey Ward (토끼 와드) - 액티브(Active / 6 rooms)
+	뒤집힌 리라로 델리리움 처치
+	사용 시 토끼 와드 설치, 설치한 위치의 맵 상 반경 3칸 범위의 방을 보여줌,
+	토끼 와드 근처의 방에 있으면 ???
+ ]]
+
+local isc = require("wakaba_src.libs.isaacscript-common")
---
+++ wakaba_src/items/1090_bookofamplitude.lua
@@ -0,0 +1,10 @@
+--[[
+	Book of Amplitude (증폭의 책) - 패시브(Passive)
+	소지 중일 때 방 입장 시마다 아래 효과 중 하나를 번갈아가며 적용
+	- 이동속도 +0.15
+	- 연사 + 1
+	- 공격력 + 2
+	- 행운 + 2
+	사용 시 차례를 다음 차례로 변경
+ ]]
+local isc = require("wakaba_src.libs.isaacscript-common")
