d16cd1dffc56ead48699b74a95be8ec850f11b35
wakaba (aka. 미카) <kanade.tinkle@gmail.com>
2023-11-26 20:30:31 +0900

    rira : kanae lens now more resembles vanilla mechanics

--- wakaba_src/items/1089_kanaelens.lua
+++ wakaba_src/items/1089_kanaelens.lua
@@ -30,15 +30,37 @@ function wakaba:FireTear_KanaeLens(tear)
 		return
 	end
 	local player = tear.Parent:ToPlayer()
-	if player and player:HasCollectible(wakaba.Enums.Collectibles.KANAE_LENS) and player:GetCollectibleNum(wakaba.Enums.Collectibles.KANAE_LENS) == 1 and tear.CanTriggerStreakEnd then
-		local fireDirection = player:GetAimDirection()
-		if fireDirection.X == 0.0 and fireDirection.Y == 0.0 then
-			fireDirection = tear.Velocity:Normalized()
-		end
-		local rightward = Vector(fireDirection.Y, -fireDirection.X)
-		local positionOffset = tear.Position - player.Position - tear.Velocity
+	wakaba:GetPlayerEntityData(player)
+	local data = player:GetData()
 
-		if positionOffset:Dot(rightward) > 0 then
+	--print(tear.PosDisplacement, tear.TearIndex, Isaac.GetFrameCount(), wakaba.G:GetFrameCount())
+	if player and player:HasCollectible(wakaba.Enums.Collectibles.KANAE_LENS) and player:GetCollectibleNum(wakaba.Enums.Collectibles.KANAE_LENS) == 1 and tear.CanTriggerStreakEnd then
+		local isPassed
+		if player:GetPlayerType() == PlayerType.PLAYER_CAIN or player:GetPlayerType() == PlayerType.PLAYER_CAIN_B or player:HasCollectible(CollectibleType.COLLECTIBLE_STAPLER) or player:HasCollectible(CollectibleType.COLLECTIBLE_MONSTROS_LUNG) then
+			local rng = player:GetCollectibleRNG(wakaba.Enums.Collectibles.KANAE_LENS)
+			if rng:RandomFloat() < 0.5 then
+				isPassed = true
+			end
+		elseif player:GetPlayerType() == PlayerType.PLAYER_KEEPER or player:GetPlayerType() == PlayerType.PLAYER_KEEPER_B or player:HasCollectible(CollectibleType.COLLECTIBLE_20_20) or player:HasCollectible(CollectibleType.COLLECTIBLE_INNER_EYE) or player:HasCollectible(CollectibleType.COLLECTIBLE_MUTANT_SPIDER) then
+			if data.wakaba.kanaeOneEye then
+				data.wakaba.kanaeOneEye = false
+				isPassed = true
+			else
+				data.wakaba.kanaeOneEye = true
+			end
+		else
+			local fireDirection = player:GetAimDirection()
+			if fireDirection.X == 0.0 and fireDirection.Y == 0.0 then
+				fireDirection = tear.Velocity:Normalized()
+			end
+			local rightward = Vector(fireDirection.Y, -fireDirection.X)
+			local positionOffset = tear.Position - player.Position - tear.Velocity
+
+			if positionOffset:Dot(rightward) > 0 then
+				isPassed = true
+			end
+		end
+		if isPassed then
 			tear:AddTearFlags(TearFlags.TEAR_HOMING)
 			tear.Color = Color(0.4, 0.15, 0.38, 1, 0.27843, 0, 0.4549)
 		end
