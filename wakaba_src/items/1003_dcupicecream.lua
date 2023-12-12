3ae75c5fdbc9d89d98268d2685fcced2a9c1f63d
wakaba (aka. 미카) <kanade.tinkle@gmail.com>
2023-12-13 02:21:29 +0900

    d-cup icecream cache fix

--- wakaba_src/items/1003_dcupicecream.lua
+++ wakaba_src/items/1003_dcupicecream.lua
@@ -1,11 +1,11 @@
-function wakaba:cacheUpdate24(player, cacheFlag)
+function wakaba:Cache_DCupIcecream(player, cacheFlag)
 	if player:HasCollectible(wakaba.Enums.Collectibles.D_CUP_ICECREAM) then
-		if cacheFlag & CacheFlag.CACHE_DAMAGE == CacheFlag.CACHE_SPEED then
+		if cacheFlag == CacheFlag.CACHE_SPEED then
 			if player:HasCollectible(CollectibleType.COLLECTIBLE_BINGE_EATER) then
 				player.MoveSpeed = player.MoveSpeed - (0.04 * player:GetCollectibleNum(wakaba.Enums.Collectibles.D_CUP_ICECREAM))
 			end
 		end
-		if cacheFlag & CacheFlag.CACHE_DAMAGE == CacheFlag.CACHE_DAMAGE then
+		if cacheFlag == CacheFlag.CACHE_DAMAGE then
 			player.Damage = (player.Damage + (0.3 * player:GetCollectibleNum(wakaba.Enums.Collectibles.D_CUP_ICECREAM) * wakaba:getEstimatedDamageMult(player))) * 1.8
 			if player:HasCollectible(CollectibleType.COLLECTIBLE_BINGE_EATER) then
 				player.Damage = player.Damage + (1 * player:GetCollectibleNum(wakaba.Enums.Collectibles.D_CUP_ICECREAM) * wakaba:getEstimatedDamageMult(player))
@@ -13,7 +13,7 @@ function wakaba:cacheUpdate24(player, cacheFlag)
 		end
 	end
 end
-wakaba:AddCallback(ModCallbacks.MC_EVALUATE_CACHE , wakaba.cacheUpdate24)
+wakaba:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, wakaba.Cache_DCupIcecream)
 
 
 
