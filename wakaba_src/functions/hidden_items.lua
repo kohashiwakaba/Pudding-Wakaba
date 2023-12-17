e7d4b124b8539693d897b1f5476bd3fbb84e246e
wakaba (aka. 미카) <kanade.tinkle@gmail.com>
2023-12-18 00:27:43 +0900

    fix hidden item issue

--- wakaba_src/functions/hidden_items.lua
+++ wakaba_src/functions/hidden_items.lua
@@ -1,5 +1,7 @@
 local isc = require("wakaba_src.libs.isaacscript-common")
 
+local inRun = false
+
 local ribbon_data = {
 	run = {
     hiddendata = {},
@@ -7,18 +9,20 @@ local ribbon_data = {
 }
 wakaba:saveDataManager("Wakaba Hidden Items", ribbon_data)
 
---local loaded = false
-function wakaba:SaveHiddenItemData()
-  ribbon_data.run.hiddendata = wakaba.HiddenItemManager:GetSaveData()
-end
-
-wakaba:AddPriorityCallback(ModCallbacks.MC_PRE_GAME_EXIT, CallbackPriority.IMPORTANT, wakaba.SaveHiddenItemData)
-
-
-function wakaba:LoadHiddenItemData()
-  if (isContinued) then
+function wakaba:LoadHiddenItemData2()
+  if inRun then return end
+  inRun = true
+  if wakaba.G:GetFrameCount() > 0 then
     wakaba.HiddenItemManager:LoadData(ribbon_data.run.hiddendata)
   end
 end
-wakaba:AddPriorityCallback(ModCallbacks.MC_POST_GAME_STARTED, CallbackPriority.LATE, wakaba.LoadHiddenItemData)
+wakaba:AddPriorityCallback(ModCallbacks.MC_POST_PLAYER_INIT, CallbackPriority.LATE, wakaba.LoadHiddenItemData2)
+
+--local loaded = false
+function wakaba:SaveHiddenItemData()
+  ribbon_data.run.hiddendata = wakaba.HiddenItemManager:GetSaveData()
+  inRun = false
+end
+
+wakaba:AddPriorityCallback(ModCallbacks.MC_PRE_GAME_EXIT, CallbackPriority.IMPORTANT, wakaba.SaveHiddenItemData)
 
