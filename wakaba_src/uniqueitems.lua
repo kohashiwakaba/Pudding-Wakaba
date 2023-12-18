7dc50c0aaae6d7377caa1922510e17f18c929b99
wakaba (aka. 미카) <37092106+kohashiwakaba@users.noreply.github.com>
2023-12-18 15:55:39 +0900

    change unique items api register flow

--- wakaba_src/uniqueitems.lua
+++ wakaba_src/uniqueitems.lua
@@ -1,21 +1,27 @@
 if UniqueItemsAPI then
-	UniqueItemsAPI.RegisterMod("Pudding and Wakaba")
-	local path = "gfx/items/collectibles/birthright/"
-
-	for _, playerName in ipairs(wakaba.Enums.UniqueItemsAppend) do
-		UniqueItemsAPI.RegisterCharacter(playerName, false)
-		UniqueItemsAPI.AddCharacterItem({
-			PlayerType = Isaac.GetPlayerTypeByName(playerName, false),
-			ItemID = CollectibleType.COLLECTIBLE_BIRTHRIGHT,
-			ItemSprite = path .. string.lower(playerName) .. "_birthright.png"
-		})
+	local isRegistered = false
+	function wakaba:Register_UniqueItems()
+		if isRegistered then return end
+		UniqueItemsAPI.RegisterMod("Pudding and Wakaba")
+		local path = "gfx/items/collectibles/birthright/"
+	
+		for _, playerName in ipairs(wakaba.Enums.UniqueItemsAppend) do
+			UniqueItemsAPI.RegisterCharacter(playerName, false)
+			UniqueItemsAPI.AddCharacterItem({
+				PlayerType = Isaac.GetPlayerTypeByName(playerName, false),
+				ItemID = CollectibleType.COLLECTIBLE_BIRTHRIGHT,
+				ItemSprite = path .. string.lower(playerName) .. "_birthright.png"
+			})
+		end
+		for _, playerName in ipairs(wakaba.Enums.UniqueItemsAppendTainted) do
+			UniqueItemsAPI.RegisterCharacter(playerName.."B", true)
+			UniqueItemsAPI.AddCharacterItem({
+				PlayerType = Isaac.GetPlayerTypeByName(playerName.."B", true),
+				ItemID = CollectibleType.COLLECTIBLE_BIRTHRIGHT,
+				ItemSprite = path .. string.lower(playerName) .. "_b_birthright.png"
+			})
+		end
+		isRegistered = true
 	end
-	for _, playerName in ipairs(wakaba.Enums.UniqueItemsAppendTainted) do
-		UniqueItemsAPI.RegisterCharacter(playerName.."B", true)
-		UniqueItemsAPI.AddCharacterItem({
-			PlayerType = Isaac.GetPlayerTypeByName(playerName.."B", true),
-			ItemID = CollectibleType.COLLECTIBLE_BIRTHRIGHT,
-			ItemSprite = path .. string.lower(playerName) .. "_b_birthright.png"
-		})
-	end
+	wakaba:AddPriorityCallback(ModCallbacks.MC_POST_PLAYER_INIT, CallbackPriority.IMPORTANT, wakaba.Register_UniqueItems)
 end
