6166f20b5513b1c14d151f6d6d64c36ae4ef5ad1
wakaba (aka. 미카) <kanade.tinkle@gmail.com>
2024-02-13 02:02:00 +0900

    rira : update hidden item manager

--- wakaba_src/libs/hidden_item_manager.lua
+++ wakaba_src/libs/hidden_item_manager.lua
@@ -1,5 +1,5 @@
 -- Hidden Item Manager, by Connor (aka Ghostbroster)
--- Version 2.0
+-- Version 2.3
 -- 
 -- Manages a system of hidden Lemegeton Item Wisps to simulate the effects of passive items without actually granting the player those items (so they can't be removed or rerolled!).
 -- Good for giving the effect of an item temporarily, making an item effect "innate" to a character, and all sorts of other stuff, probably.
@@ -27,42 +27,6 @@ local kPersistentWispMarker = 617413666
 local kEarlyCallbackPriority = -9999
 local kLateCallbackPriority = 9999
 
---------------------------------------------------
--- Initialization
-
-local Callbacks = {}
-
-local function AddCallback(callbackID, func, param, priority)
-	table.insert(Callbacks, {
-		Callback = callbackID,
-		Func = func,
-		Param = param,
-		Priority = priority or kEarlyCallbackPriority,
-	})
-end
-local function AddLateCallback(callbackID, func, param)
-	AddCallback(callbackID, func, param, kLateCallbackPriority)
-end
-
-local initialized = false
-function HiddenItemManager:Init(mod)
-	if not initialized then
-		HiddenItemManager.Mod = mod
-		
-		for _, tab in ipairs(Callbacks) do
-			mod:AddPriorityCallback(tab.Callback, tab.Priority, tab.Func, tab.Param)
-		end
-		
-		HiddenItemManager.WispTag = "HiddenItemManager:" .. mod.Name
-		
-		initialized = true
-	end
-	return HiddenItemManager
-end
-
---------------------------------------------------
--- Storage/Utility
-
 local function LOG_ERROR(str)
 	local prefix = ""
 	if HiddenItemManager.Mod then
@@ -81,6 +45,42 @@ local function LOG(str)
 	local fullStr = "[" .. prefix .. "HiddenItemManager]: " .. str
 	Isaac.DebugString(fullStr)
 end
+
+--------------------------------------------------
+-- Initialization
+
+local Callbacks = {}
+
+local function AddCallback(callbackID, func, param, priority)
+	table.insert(Callbacks, {
+		Callback = callbackID,
+		Func = func,
+		Param = param,
+		Priority = priority or kEarlyCallbackPriority,
+	})
+end
+local function AddLateCallback(callbackID, func, param)
+	AddCallback(callbackID, func, param, kLateCallbackPriority)
+end
+
+function HiddenItemManager:Init(mod)
+	HiddenItemManager.Mod = mod
+	HiddenItemManager.WispTag = "HiddenItemManager:" .. mod.Name
+	
+	if not mod.AddedHiddenItemManagerCallbacks then
+		for _, tab in ipairs(Callbacks) do
+			mod:AddPriorityCallback(tab.Callback, tab.Priority, tab.Func, tab.Param)
+		end
+		mod.AddedHiddenItemManagerCallbacks = true
+	else
+		LOG_ERROR("More than one instance initialized!")
+	end
+	
+	return HiddenItemManager
+end
+
+--------------------------------------------------
+-- Storage/Utility
 
 local kDefaultGroup = "HIDDEN_ITEM_MANAGER_DEFAULT"
 
@@ -103,6 +103,9 @@ local INDEX = {}
 
 -- Cache for EntityPtrs to wisps.
 local WISP_PTRS = {}
+
+-- Groups that should not apply costumes from wisps.
+local NO_COSTUME_GROUPS = {}
 
 -- Removes all empty subtables from a given table.
 local function CleanUp(tab)
@@ -130,9 +133,6 @@ local function FindData(playerKey, group, itemID, allowInit)
 			return
 		end
 		DATA[playerKey] = {}
-	end
-	if not group then
-		return DATA[playerKey]
 	end
 	if not DATA[playerKey][group] then
 		if not allowInit then
@@ -313,10 +313,30 @@ local function InitializeWisp(wisp)
 	WISP_PTRS[wispKey] = EntityPtr(wisp)
 	tab.PlayerKey = GetPlayerKey(wisp.Player)
 	tab.Initialized = true
+	
+	if NO_COSTUME_GROUPS[tab.Group] and not wisp.Player:HasCollectible(wisp.SubType, true) then
+		wisp.Player:RemoveCostume(Isaac.GetItemConfig():GetCollectible(wisp.SubType))
+	end
+end
+
+-- Removes item costumes from players if they should be hidden.
+function HiddenItemManager:CheckCostumes()
+	for key, data in pairs(INDEX) do
+		local wisp = GetWisp(data)
+		local player = GetPlayer(data)
+		if wisp and player then
+			if NO_COSTUME_GROUPS[data.Group] and not player:HasCollectible(wisp.SubType, true) then
+				player:RemoveCostume(Isaac.GetItemConfig():GetCollectible(wisp.SubType))
+			end
+		end
+	end
 end
 
 -- Spawns a hidden item wisp.
 local function SpawnWisp(player, itemID, duration, group, removeOnNewRoom, removeOnNewLevel)
+	if not HiddenItemManager.Mod then
+		LOG_ERROR("Not initialized! Did you forget to call `hiddenItemManager:Init(mod)`?")
+	end
 	group = GetGroup(group)
 	if not itemID or itemID < 1 then
 		LOG_ERROR("Attempted to add invalid CollectibleType `" .. (itemID or "NULL") .. "` to group: " .. group)
@@ -351,6 +371,12 @@ end
 
 --------------------------------------------------
 -- API Functions
+
+-- Hide costumes for any wisps added in the specified group.
+-- Will hide costumes for wisps in the default group if unspecified.
+function HiddenItemManager:HideCostumes(group)
+	NO_COSTUME_GROUPS[GetGroup(group)] = true
+end
 
 -- Add a hidden item(s) that will persist through room and floor transitions.
 function HiddenItemManager:Add(player, itemID, duration, numToAdd, group)
@@ -407,10 +433,9 @@ end
 
 -- Removes all hidden items from the specified group.
 function HiddenItemManager:RemoveAll(player, group)
-	group = GetGroup(group)
-	local pKey = GetPlayerKey(player)
-	if DATA[pKey] then
-		for itemID, wispList in pairs(DATA[pKey][group]) do
+	local tab = FindData(GetPlayerKey(player), group)
+	if tab then
+		for itemID, wispList in pairs(tab) do
 			for wispKey, _ in pairs(wispList) do
 				RemoveWisp(wispKey)
 			end
@@ -442,10 +467,12 @@ function HiddenItemManager:GetStacks(player, group)
 	
 	local output = {}
 	
-	for itemID, _ in pairs(tab) do
-		local count = HiddenItemManager:CountStack(player, itemID, group)
-		if count > 0 then
-			output[itemID] = count
+	if tab then
+		for itemID, _ in pairs(tab) do
+			local count = HiddenItemManager:CountStack(player, itemID, group)
+			if count > 0 then
+				output[itemID] = count
+			end
 		end
 	end
 	
@@ -486,12 +513,14 @@ function HiddenItemManager:LoadData(saveData)
 	end
 	DATA = {}
 	HiddenItemManager.INITIALIZING = false
-	for _, ptr in pairs(WISP_PTRS) do
+	-- Check & re-initialize all existing wisps, just in case.
+	local oldPtrs = WISP_PTRS
+	WISP_PTRS = {}
+	for _, ptr in pairs(oldPtrs) do
 		if ptr and ptr.Ref then
 			HiddenItemManager:ItemWispUpdate(ptr.Ref:ToFamiliar())
 		end
 	end
-	WISP_PTRS = {}
 	HiddenItemManager:CheckWisps()
 end
 
@@ -511,8 +540,9 @@ function HiddenItemManager:ItemWispUpdate(wisp)
 		local player = wisp.Player
 		local playerKey = GetPlayerKey(player)
 		
-		if not IsManagedWisp(wisp) then
+		if not IsManagedWisp(wisp) or not WISP_PTRS[wispKey] then
 			-- This wisp isn't marked as one of our wisps, but we're supposed to have a wisp with this InitSeed.
+			-- OR: we don't have a pointer to this wisp cached, meaning we may have reloaded a save or something.
 			
 			-- Check if there's already an active wisp for this effect.
 			local existingWisp = GetWisp(wispData)
@@ -524,7 +554,8 @@ function HiddenItemManager:ItemWispUpdate(wisp)
 				return false
 			end
 			
-			-- Most likely, we've quit and continued a run. Re-initialize this wisp as a hidden one.
+			-- Most likely, we've quit and continued a run, reloaded a save, or luamodded, or something.
+			-- Re-initialize this wisp as a hidden one.
 			InsertData(playerKey, wispData.Group, wispData.Item, wispKey, wispData)
 			InitializeWisp(wisp)
 		end
@@ -659,6 +690,8 @@ function HiddenItemManager:PostNewRoom()
 			data.ErrorCount = 0
 		end
 	end
+	
+	HiddenItemManager:CheckCostumes()
 end
 AddCallback(ModCallbacks.MC_POST_NEW_ROOM, HiddenItemManager.PostNewRoom)
 
