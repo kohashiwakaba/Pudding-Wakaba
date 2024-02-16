b001631ff52fa57b5f36766cb3c09fbc7af0a47c
wakaba (aka. 미카) <kanade.tinkle@gmail.com>
2024-02-17 01:11:55 +0900

    rira : richer's bra extra

--- wakaba_src/items/1100_richerbra.lua
+++ wakaba_src/items/1100_richerbra.lua
@@ -10,3 +10,23 @@ function wakaba:AlterPlayerDamage_RicherBra(player, amount, flags, source, count
 	end
 end
 wakaba:AddCallback(wakaba.Callback.EVALUATE_DAMAGE_AMOUNT, wakaba.AlterPlayerDamage_RicherBra)
+
+function wakaba:NewRoom_RicherBra()
+	local room = wakaba.G:GetRoom()
+	if room:IsFirstVisit() and wakaba:AnyPlayerHasCollectible(wakaba.Enums.Collectibles.RICHERS_BRA) then
+		for i=0, room:GetGridSize() - 1 do
+			local gridEnt = room:GetGridEntity(i)
+			if gridEnt then
+				local plate = gridEnt:ToPressurePlate()
+				if plate and plate:GetVariant() == 0 then
+					wakaba.Log("Richer's Bra - Pressure plate found : pressing...")
+					plate.State = 3
+					plate:GetSprite():Play("On", true)
+					plate:Update()
+					plate:GetSprite():Update()
+				end
+			end
+		end
+	end
+end
+wakaba:AddCallback(ModCallbacks.MC_POST_NEW_ROOM, wakaba.NewRoom_RicherBra)
