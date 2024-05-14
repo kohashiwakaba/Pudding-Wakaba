730ea3e4cc3d5cc2370a36e50cc92cf8a6c8af99
wakaba (aka. 미카) <kanade.tinkle@gmail.com>
2024-05-15 08:35:32 +0900

    lunatic mode invalidates godhead unnerf

--- wakaba_src/functions/take_damage_npc.lua
+++ wakaba_src/functions/take_damage_npc.lua
@@ -58,6 +58,22 @@ function wakaba:TakeDamage_Global(target, damage, flags, source, countdown)
 		end
 		]]
 
+		if wakaba:IsLunatic() then
+			if damage == 2 and source.Entity and source.Entity.Type == 2 and target.Type ~= 1 then
+				local player = source.Entity.SpawnerEntity:ToPlayer()
+				if player == nil then
+					player = source.Entity.SpawnerEntity:ToFamiliar().Player
+				end
+				local tear = source.Entity:ToTear()
+				local tf = tear.TearFlags
+				if player and tf|TearFlags.TEAR_GLOW == tf then
+					wakaba.Log("Found Godhead Damage! replacing...")
+					newDamage = damage + 0.0001
+					sendNewDamage = true
+				end
+			end
+		end
+
 		local returndata = wakaba:RiraBraOnDamage(source, target, data, newDamage, newFlags)
 		newDamage = returndata.newDamage or newDamage
 		sendNewDamage = returndata.sendNewDamage or sendNewDamage
@@ -198,4 +214,4 @@ function wakaba:TakeDamage_Global(target, damage, flags, source, countdown)
 	end
 end
 
-wakaba:AddCallback(ModCallbacks.MC_ENTITY_TAKE_DMG, wakaba.TakeDamage_Global)
+wakaba:AddPriorityCallback(ModCallbacks.MC_ENTITY_TAKE_DMG, CallbackPriority.IMPORTANT, wakaba.TakeDamage_Global)
