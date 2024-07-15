d2ddf1c696c3a6bb754231c307eac90a5abca3ff
wakaba (aka. 미카) <37092106+kohashiwakaba@users.noreply.github.com>
2024-07-15 15:54:10 +0900

    Update samael.lua

--- wakaba_src/compat/samael.lua
+++ wakaba_src/compat/samael.lua
@@ -1,7 +1,7 @@
 
 local isc = require("wakaba_src.libs.isaacscript-common")
 
-wakaba:RegisterPatch(0, "SamaelMod", function() return (SacredDreams ~= nil) end, function()
+wakaba:RegisterPatch(0, "SamaelMod", function() return (SamaelMod ~= nil) end, function()
 	do
 		wakaba:BlacklistUniform("card", SamaelMod.ITEMS.DENIAL_DICE)
 
