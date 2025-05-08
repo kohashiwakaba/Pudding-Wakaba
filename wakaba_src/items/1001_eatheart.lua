16f8f5eb5862f5f40345e1875cbc07d4bff86983
리셰/richer-chan <kanade.tinkle@gmail.com>
2025-05-09 00:32:32 +0900

    prevent eat heart spawning additional pedestal with car battery

--- wakaba_src/items/1001_eatheart.lua
+++ wakaba_src/items/1001_eatheart.lua
@@ -1,8 +1,10 @@
 
 ---@param player EntityPlayer
 function wakaba:PreUseItem_EatHeart(_, rng, player, useFlags, activeSlot, varData)
-	if activeSlot < 0 and useFlags & UseFlag.USE_VOID == 0 and useFlags & UseFlag.USE_CARBATTERY == 0 then
-		player:UseActiveItem(CollectibleType.COLLECTIBLE_METRONOME, UseFlag.USE_VOID, -1)
+	if activeSlot < 0 and useFlags & UseFlag.USE_VOID == 0 then
+		if useFlags & UseFlag.USE_CARBATTERY == 0 then
+			player:UseActiveItem(CollectibleType.COLLECTIBLE_METRONOME, UseFlag.USE_VOID, -1)
+		end
 	else
 		if useFlags & UseFlag.USE_VOID == UseFlag.USE_VOID then
 			player:GetData().wakaba.eatheartquality = 1
