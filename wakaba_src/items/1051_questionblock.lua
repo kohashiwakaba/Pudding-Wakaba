46c6b6f75956b87ea4555d8c6b1cbba1a866bceb
리셰/richer-chan <kanade.tinkle@gmail.com>
2025-05-12 02:06:31 +0900

    question block test 2

--- wakaba_src/items/1051_questionblock.lua
+++ wakaba_src/items/1051_questionblock.lua
@@ -22,31 +22,37 @@ if not REPENTOGON then return end
 
 -- TODO
 wakaba.QuestionBlockRewards = {
-  {e = EntityType.ENTITY_PICKUP, v = PickupVariant.PICKUP_COIN, s = CoinSubType.COIN_PENNY, x = wakaba.Enums.SoundEffects.CHIMAKI_KYUU, w = 3.0, sf = 300},
-  {e = EntityType.ENTITY_PICKUP, v = PickupVariant.PICKUP_COIN, s = CoinSubType.COIN_NICKEL,  x = wakaba.Enums.SoundEffects.CHIMAKI_KYUU, w = 0.1},
-  {e = EntityType.ENTITY_PICKUP, v = PickupVariant.PICKUP_COIN, s = CoinSubType.COIN_DIME,  x = wakaba.Enums.SoundEffects.CHIMAKI_KYUU, w = 0.04},
-  {e = EntityType.ENTITY_PICKUP, v = PickupVariant.PICKUP_COIN, s = CoinSubType.COIN_LUCKYPENNY,  x = wakaba.Enums.SoundEffects.CHIMAKI_KYUU, w = 0.06},
-  {e = EntityType.ENTITY_PICKUP, v = PickupVariant.PICKUP_COIN, s = CoinSubType.COIN_GOLDEN,  x = wakaba.Enums.SoundEffects.CHIMAKI_KYUU, w = 0.03},
-  {e = EntityType.ENTITY_PICKUP, v = PickupVariant.PICKUP_COIN, s = wakaba.Enums.Coins.EASTER_EGG,  x = wakaba.Enums.SoundEffects.CHIMAKI_KYUU, w = 0.1},
-  {e = EntityType.ENTITY_PICKUP, v = PickupVariant.PICKUP_KEY,  s = KeySubType.KEY_NORMAL , x = wakaba.Enums.SoundEffects.CHIMAKI_KYUU, w = 0.4},
-  {e = EntityType.ENTITY_PICKUP, v = PickupVariant.PICKUP_BOMB, s = BombSubType.BOMB_NORMAL, x = wakaba.Enums.SoundEffects.CHIMAKI_KYUU, w = 0.4},
-  {e = EntityType.ENTITY_PICKUP, v = PickupVariant.PICKUP_TAROTCARD, s = -1, x = wakaba.Enums.SoundEffects.CHIMAKI_KYUU, w = 0.2},
-  {e = EntityType.ENTITY_PICKUP, v = PickupVariant.PICKUP_PILL, s = -1, x = wakaba.Enums.SoundEffects.CHIMAKI_KYUU, w = 0.2},
-  {e = EntityType.ENTITY_PICKUP, v = PickupVariant.PICKUP_GRAB_BAG, s = SackSubType.SACK_NORMAL, x = wakaba.Enums.SoundEffects.CHIMAKI_KYUU, w = 0.1},
-  {e = EntityType.ENTITY_PICKUP, v = PickupVariant.PICKUP_THROWABLEBOMB, s = -1, x = wakaba.Enums.SoundEffects.CHIMAKI_KYUU, w = 0.5},
-  {e = EntityType.ENTITY_PICKUP, v = PickupVariant.PICKUP_TRINKET, s = -1, x = wakaba.Enums.SoundEffects.CHIMAKI_KYUU, w = 0.02},
-  {e = EntityType.ENTITY_PICKUP, v = PickupVariant.PICKUP_COLLECTIBLE, s = CollectibleType.COLLECTIBLE_MAGIC_MUSHROOM, x = wakaba.Enums.SoundEffects.CHIMAKI_KYUU, w = 0.05},
-  {e = EntityType.ENTITY_PICKUP, v = PickupVariant.PICKUP_COLLECTIBLE, s = CoinSubType.COLLECTIBLE_1UP, x = wakaba.Enums.SoundEffects.CHIMAKI_KYUU, w = 0.01},
-  {e = EntityType.ENTITY_PICKUP, v = PickupVariant.PICKUP_COLLECTIBLE, s = CoinSubType.COLLECTIBLE_MINI_MUSH, x = wakaba.Enums.SoundEffects.CHIMAKI_KYUU, w = 0.01},
-  {e = EntityType.ENTITY_PICKUP, v = PickupVariant.PICKUP_COLLECTIBLE, s = CoinSubType.COLLECTIBLE_MEGA_MUSH, x = wakaba.Enums.SoundEffects.CHIMAKI_KYUU, w = 0.0001},
-  {e = EntityType.ENTITY_PICKUP, v = PickupVariant.PICKUP_COLLECTIBLE, s = CoinSubType.COLLECTIBLE_GNAWED_LEAF, x = wakaba.Enums.SoundEffects.CHIMAKI_KYUU, w = 0.01},
-  {e = EntityType.ENTITY_PICKUP, v = PickupVariant.PICKUP_COLLECTIBLE, s = CoinSubType.COLLECTIBLE_BOBBY_BOMB, x = wakaba.Enums.SoundEffects.CHIMAKI_KYUU, w = 0.01},
-  {e = EntityType.ENTITY_PICKUP, v = PickupVariant.PICKUP_COLLECTIBLE, s = CoinSubType.COLLECTIBLE_GLITCHED_CROWN, x = wakaba.Enums.SoundEffects.CHIMAKI_KYUU, w = 0.0001},
+  {e = EntityType.ENTITY_PICKUP, v = PickupVariant.PICKUP_COIN, s = CoinSubType.COIN_PENNY, x = SoundEffect.SOUND_PENNYPICKUP, w = 1.0},
+  {e = EntityType.ENTITY_PICKUP, v = PickupVariant.PICKUP_COIN, s = CoinSubType.COIN_NICKEL,  x = SoundEffect.SOUND_PENNYPICKUP, w = 0.1},
+  {e = EntityType.ENTITY_PICKUP, v = PickupVariant.PICKUP_COIN, s = CoinSubType.COIN_DIME,  x = SoundEffect.SOUND_PENNYPICKUP, w = 0.04},
+  {e = EntityType.ENTITY_PICKUP, v = PickupVariant.PICKUP_COIN, s = CoinSubType.COIN_LUCKYPENNY,  x = SoundEffect.SOUND_PENNYPICKUP, w = 0.06},
+  {e = EntityType.ENTITY_PICKUP, v = PickupVariant.PICKUP_COIN, s = CoinSubType.COIN_GOLDEN,  x = SoundEffect.SOUND_PENNYPICKUP, w = 0.03},
+  {e = EntityType.ENTITY_PICKUP, v = PickupVariant.PICKUP_COIN, s = wakaba.Enums.Coins.EASTER_EGG,  x =  wakaba.Enums.SoundEffects.CHIMAKI_KYUU, w = 0.1},
+  {e = EntityType.ENTITY_PICKUP, v = PickupVariant.PICKUP_KEY,  s = KeySubType.KEY_NORMAL , x = SoundEffect.SOUND_PLOP, w = 0.4},
+  {e = EntityType.ENTITY_PICKUP, v = PickupVariant.PICKUP_BOMB, s = BombSubType.BOMB_NORMAL, x = SoundEffect.SOUND_PLOP, w = 0.4},
+  {e = EntityType.ENTITY_PICKUP, v = PickupVariant.PICKUP_TAROTCARD, s = 0, x = SoundEffect.SOUND_SHELLGAME, w = 0.2},
+  {e = EntityType.ENTITY_PICKUP, v = PickupVariant.PICKUP_PILL, s = 0, x = SoundEffect.SOUND_SHELLGAME, w = 0.2},
+  {e = EntityType.ENTITY_PICKUP, v = PickupVariant.PICKUP_GRAB_BAG, s = SackSubType.SACK_NORMAL, x = SoundEffect.SOUND_SHELLGAME, w = 0.1},
+  {e = EntityType.ENTITY_PICKUP, v = PickupVariant.PICKUP_THROWABLEBOMB, s = 0, x = SoundEffect.SOUND_SHELLGAME, w = 0.5},
+  {e = EntityType.ENTITY_PICKUP, v = PickupVariant.PICKUP_TRINKET, s = 0, x = SoundEffect.SOUND_SHELLGAME, w = 0.02},
+  {e = EntityType.ENTITY_PICKUP, v = PickupVariant.PICKUP_COLLECTIBLE, s = CollectibleType.COLLECTIBLE_MAGIC_MUSHROOM, x = SoundEffect.SOUND_MUSHROOM_POOF_2, w = 0.05},
+  {e = EntityType.ENTITY_PICKUP, v = PickupVariant.PICKUP_COLLECTIBLE, s = CollectibleType.COLLECTIBLE_1UP, x = SoundEffect.SOUND_MUSHROOM_POOF_2, w = 0.01},
+  {e = EntityType.ENTITY_PICKUP, v = PickupVariant.PICKUP_COLLECTIBLE, s = CollectibleType.COLLECTIBLE_MINI_MUSH, x = SoundEffect.SOUND_MUSHROOM_POOF_2, w = 0.01},
+  {e = EntityType.ENTITY_PICKUP, v = PickupVariant.PICKUP_COLLECTIBLE, s = CollectibleType.COLLECTIBLE_MEGA_MUSH, x = SoundEffect.SOUND_MUSHROOM_POOF_2, w = 0.0001},
+  {e = EntityType.ENTITY_PICKUP, v = PickupVariant.PICKUP_COLLECTIBLE, s = CollectibleType.COLLECTIBLE_GNAWED_LEAF, x = SoundEffect.SOUND_MUSHROOM_POOF_2, w = 0.01},
+  {e = EntityType.ENTITY_PICKUP, v = PickupVariant.PICKUP_COLLECTIBLE, s = CollectibleType.COLLECTIBLE_BOBBY_BOMB, x = SoundEffect.SOUND_MUSHROOM_POOF_2, w = 0.01},
+  {e = EntityType.ENTITY_PICKUP, v = PickupVariant.PICKUP_COLLECTIBLE, s = CollectibleType.COLLECTIBLE_GLITCHED_CROWN, x = SoundEffect.SOUND_MUSHROOM_POOF_2, w = 0.0001},
 }
 
 wakaba.QuestionBlockWeight = WeightedOutcomePicker()
 for i, e in ipairs(wakaba.QuestionBlockRewards) do
-  wakaba.QuestionBlockWeight:AddOutcomeFloat(i, e.w, e.sf or 100)
+  wakaba.QuestionBlockWeight:AddOutcomeFloat(i, e.w)
+end
+
+---@param grid GridEntity
+---@return integer
+function wakaba:getDesignatedQuestionBlockSeed(grid)
+  return (wakaba.G:GetRoom():GetDecorationSeed() // 2) + (grid:GetGridIndex() * 266543457) - (grid.Position.X * 55325) + (grid.Position.Y * 654342)
 end
 
 ---@param questionBlock GridEntity
@@ -60,7 +66,7 @@ function wakaba:loadQuestionBlockSprite(questionBlock)
     anm2 = "gfx/grid/wakaba_question_block_used.anm2"
   end
   sprite:Load(anm2, true)
-  sprite:SetFrame(animation, frame)
+  sprite:SetFrame(anim, frame)
 end
 
 function wakaba:NewRoom_QuestionBlock()
@@ -99,10 +105,10 @@ function wakaba:HitQuestionBlock(questionBlock)
   if questionBlock.VarData == 1 then
     return
   end
-  questionBlock.VarData = 1
-  wakaba:loadQuestionBlockSprite(gridEnt)
-  questionBlock:Update()
   wakaba:spawnQuestionBlockReward(questionBlock)
+  questionBlock.VarData = 1
+  wakaba:loadQuestionBlockSprite(questionBlock)
+  questionBlock:Update()
 end
 
 ---@param questionBlock GridEntity
@@ -115,36 +121,42 @@ function wakaba:spawnQuestionBlockReward(questionBlock)
     return
   end
 
-  local rng = questionBlock:GetRNG()
+  local seed = wakaba:getDesignatedQuestionBlockSeed(questionBlock)
+  local rng = RNG(seed)
   local outcome = wakaba.QuestionBlockWeight:PickOutcome(rng)
   local entry = wakaba.QuestionBlockRewards[outcome]
-  if entry.sf then
-    SFXManager():Play(entry.sf)
+  SFXManager():Play(SoundEffect.SOUND_POT_BREAK)
+  if entry.x then
+    SFXManager():Play(entry.x)
   end
 
   if entry.e == EntityType.ENTITY_PICKUP and entry.v == PickupVariant.PICKUP_COLLECTIBLE then
     local itemPedestal = Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, entry.s, questionBlock.Position, Vector.Zero, nil):ToPickup()
     questionBlock:Destroy()
   else
-    local entity = Isaac.Spawn(entry.e, entry.v, entry.s, wakaba.R().FindFreePickupSpawnPosition(questionBlock.Position), Vector.Zero, nil):ToPickup()
+    local entity = Isaac.Spawn(entry.e, entry.v, entry.s, wakaba.R():FindFreePickupSpawnPosition(questionBlock.Position), Vector.Zero, nil):ToPickup()
   end
 end
 
-function wakaba:PreGridSpawn_QuestionBlock(type, variant, varData, gridIdx, seed, desc)
-  if not (type == GridEntityType.GRID_ROCK and variant == wakaba.Enums.GridVars.QUESTION_BLOCK) then
+---@param grid GridEntity
+function wakaba:PostGridSpawn_QuestionBlock(grid)
+  if not (grid:GetType() == GridEntityType.GRID_ROCK and grid:GetVariant() ~= wakaba.Enums.GridVars.QUESTION_BLOCK) then
     return
   end
   local count = PlayerManager.GetNumCollectibles(wakaba.Enums.Collectibles.QUESTION_BLOCK)
   if count > 0 then
-    local rng = RNG(seed)
-    if rng:PhantomFloat() <= 0.001 then
-      return {
-        Variant = wakaba.Enums.GridVars.QUESTION_BLOCK
-      }
+    local seed = (wakaba.G:GetRoom():GetDecorationSeed() // 2) + (grid:GetGridIndex() * 266543457) - (grid.Position.X * 55325) + (grid.Position.Y * 654342)
+
+    local rng = RNG(seed, 35)
+    local res = rng:PhantomFloat()
+    wakaba.Log("Question block test from gridIdx", grid:GetGridIndex(), "seed", seed, "res", res)
+    if res <= wakaba.Enums.Constants.QUESTION_BLOCK_SPAWN_RATE then
+      wakaba.Log("Question block spawned! from gridIdx", grid:GetGridIndex(), "seed", seed)
+      grid:SetVariant(wakaba.Enums.GridVars.QUESTION_BLOCK)
     end
   end
 end
-wakaba:AddCallback(ModCallbacks.MC_PRE_GRID_ENTITY_SPAWN, wakaba.PreGridSpawn_QuestionBlock, GridEntityType.GRID_ROCK)
+wakaba:AddCallback(ModCallbacks.MC_POST_GRID_ENTITY_SPAWN, wakaba.PostGridSpawn_QuestionBlock)
 
 ---@param tear EntityTear
 ---@param gridIndex integer
